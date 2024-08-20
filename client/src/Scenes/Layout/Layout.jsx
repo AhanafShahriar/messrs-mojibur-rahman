9b794224bbda169dd8b91652996a8d0cb46dbadc
dev-Mostafizur <mostafizurwork899@gmail.com>
2024-08-21 00:36:17 +0600

    FlexBetween page added for css style

--- client/src/Scenes/Layout/Layout.jsx
+++ client/src/Scenes/Layout/Layout.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useState } from 'react';
+import {Box, useMediaQuery} from "@mui/material"
+import { useSelector } from 'react-redux';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
     return (
         <>
-        <h1>layout</h1>
+     <Box width="100%" height="100%">
+        <Box>
+
+            <Outlet></Outlet>
+        </Box>
+     </Box>
         </>
     );
 };
